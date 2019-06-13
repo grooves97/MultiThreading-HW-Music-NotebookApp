df7ebae7d6d87b70ece8fdd77a4302d42570b9e6
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-13 11:54:21 +0600

    BankAccount

---
+++ ThreadBankSystem/ThreadBankSystem.Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadBankSystem.Models
+{
+    [Synchronization]
+    public class User : ContextBoundObject
+    {
+        private object lockObject = new object();
+        public string Fullname { get; set; }
+        public int Sum { get; private set; } = 3000;
+
+        public void AddSum(int sum)
+        {
+            Monitor.Enter(lockObject);
+            try
+            {
+
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+
+        public int WithDrawSum(int sum)
+        {
+
+        }
+
+    }
+}
